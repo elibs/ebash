c177339f9d1fa3ec0b028f484ca5b90dfa32d0d8
Marshall McMullen <marshall@solidfire.com>
2015-06-25 01:45:53 -0600

    Add set -o errtrace to ensure ERR trap is auto inherited by all functions, command substitutions, and commands executed in a subshell environment.  The ERR trap is normally not inherited in such cases.

--- unittest/eretry.sh
+++ unittest/eretry.sh
@@ -9,8 +9,7 @@ fail_then_pass()
 
 ETEST_eretry_preserve_exit_code()
 {
-    eretry -r=3 fail_then_pass 3
-    assert_eq 15 $?
+    eretry -r=3 fail_then_pass 3 && die "eretry should abort" || assert_eq 15 $?
 
     # Ensure fail_then_pass was actually called specified number of times
     assert_eq 3 ${FAIL_TIMES}
@@ -27,33 +26,31 @@ ETEST_eretry_fail_till_last()
 
 ETEST_eretry_exit_124_on_timeout()
 {
-    eretry -r=0 -t=0.1s sleep 3
-    assert_eq 124 $?
+    eretry -r=1 -t=0.1s sleep 3 && die "eretry should abort" || assert_eq 124 $?
 }
 
 ETEST_eretry_warn_every()
 {
     EDEBUG=
 
-    output=$(eretry -r=10 -w=2 false 2>&1)
+    output=$(eretry -r=10 -w=2 false 2>&1 || true)
     einfo "$(lval output)"
     assert_eq 5 $(echo "$output" | wc -l)
 
-    output=$(eretry -r=30 -w=3 false 2>&1)
+    output=$(eretry -r=30 -w=3 false 2>&1 || true)
     einfo "$(lval output)"
     assert_eq 10 $(echo "$output" | wc -l)
 
-    output=$(eretry -r=3 -w=1 false 2>&1)
+    output=$(eretry -r=3 -w=1 false 2>&1 || true)
     einfo "$(lval output)"
     assert_eq 3 $(echo "$output" | wc -l)
 
-    output=$(eretry -r=0 -w=1 false 2>&1)
+    output=$(eretry -r=0 -w=1 false 2>&1 || true)
     einfo "$(lval output)"
     assert_eq 1 $(echo "$output" | wc -l)
 }
 
 ETEST_eretry_hang()
 {
-    eretry -t=1s -r=0 sleep infinity
-    assert_eq 124 $?
+    eretry -t=1s -r=0 sleep infinity && die "eretry should abort" || assert_eq 124 $?
 }
