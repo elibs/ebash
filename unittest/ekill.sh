644acf0bf873c0c93f5511c296fc4fb558828e22
Marshall McMullen <marshall@solidfire.com>
2015-07-03 01:17:29 -0600

    Add simple die unit test

--- unittest/ekill.sh
+++ unittest/ekill.sh
@@ -48,3 +48,25 @@ ETEST_ekilltree()
         ! process_running ${pid} || die "${pid} failed to exit"
     done
 }
+
+ETEST_die()
+{
+    try
+    {
+        # Create a bunch of background processes
+        sleep infinity& echo "$!" >> pids
+        yes >/dev/null& echo "$!" >> pids
+        bash -c 'sleep 1000' & echo "$!" >> pids
+        bash -c 'sleep 4000' & echo "$!" >> pids
+
+        die
+    }
+    catch
+    {
+        true
+    }
+
+    for pid in $(cat pids); do
+        eretry -t=2s "process_not_running ${pid}" || die "${pid} failed to get killed by die"
+    done
+}
