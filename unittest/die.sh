6d6a25f7ff8a0af1ffbf531a9a5e62c9a614286d
Marshall McMullen <marshall@solidfire.com>
2015-09-11 02:09:48 -0600

    Fix many many tiny bugs to make die work properly...

--- unittest/die.sh
+++ unittest/die.sh
@@ -5,12 +5,15 @@ ETEST_die_subprocesses()
     try
     {
         # Create a bunch of background processes
-        sleep infinity& echo "$!" >> pids
-        yes >/dev/null& echo "$!" >> pids
+        sleep infinity& echo "$!"        >> pids
+        sleep infinity& echo "$!"        >> pids
         bash -c 'sleep 1000' & echo "$!" >> pids
         bash -c 'sleep 4000' & echo "$!" >> pids
 
-        die
+        einfo "Processes are running..."
+        cat pids
+
+        die "Killing try block"
     }
     catch
     {
@@ -18,7 +21,7 @@ ETEST_die_subprocesses()
     }
 
     for pid in $(cat pids); do
-        eretry -t=2s process_not_running ${pid} || die "${pid} failed to get killed by die"
+        eretry -t=2s process_not_running ${pid}
     done
 }
 
