23160632c288e9212006f9efc3e1a6b9004f04ec
Michael Odell <michael@theodells.us>
2016-01-14 13:28:30 -0700

    Clean up the code that replaces gnu tool names with functions pointing to their g-prefixed versions for non-Linux OSes.

--- platform.sh
+++ platform.sh
@@ -3,7 +3,6 @@
 # Copyright 2011-2015, SolidFire, Inc. All rights reserved.
 #
 
-
 # We presently assume that linux boxes will have a proper gnu toolchain in
 # the default path.  For them, nothing need be done.
 [[ ${__BU_OS} == "Linux" ]] && return 0
@@ -14,58 +13,41 @@
 # grep gets installed as ggrep.
 #
 # This would probably be a nice area to allow for configuration, but for
-# now we assume things meet that assumption and that the gnu tools with a
-# "g" prepended to their name are in the path.
+# now we assume that the GNU toolchain is installed in that fashion on anything
+# that is not Linux.  (GNU/Linux? ;-)
 #
 #
-#__BU_GNU_TOOLS=(
-#    readlink
-#    find
-#    sort
-#    tr
-#    dd
-#    sed
-#    grep
-#    tar
-#    cp
-#    mktemp
-#    stat
-#    sleep
-#    timeout
-#    cat
-#    tee
-#    base64
-#    md5sum
-#)
-#
-#redirect_gnu_tools()
-#{
-#    local tool
-#    for tool in "${__BU_GNU_TOOLS[@]}" ; do
-#
-#    done
-#}
+__BU_GNU_TOOLS=(
+    base64
+    cat
+    cp
+    dd
+    find
+    grep
+    md5sum
+    mktemp
+    readlink
+    sed
+    sha256sum
+    sleep
+    sort
+    stat
+    tar
+    tee
+    timeout
+    tr
+)
 
+# Make a function to take the place of every GNU tool in __BU_GNU_TOOLS that
+# calls the g-prefixed version of the same gnu tool
+redirect_gnu_tools()
+{
+    local tool
+    for tool in "${__BU_GNU_TOOLS[@]}" ; do
 
-# TODO modell sha256sum is special
-    sha256sum() { shasum "${@}" ; }
+        eval "function ${tool} { g${tool} \"\${@}\" ; }"
 
-# TODO modell rest of the gnu tools that bashutils uses
-    readlink() { greadlink "${@}" ; }
-    find() { gfind "${@}" ; }
-    sort() { gsort "${@}" ; }
-    tr() { gtr "${@}" ; }
-    dd() { gdd "${@}" ; }
-    sed() { gsed "${@}" ; }
-    grep() { ggrep "${@}" ; }
-    tar() { gtar "${@}" ; }
-    cp() { gcp "${@}" ; }
-    mktemp() { gmktemp "${@}" ; }
-    stat() { gstat "${@}" ; }
-    sleep() { gsleep "${@}" ; }
-    timeout() { gtimeout "${@}" ; }
-    cat() { gcat "${@}" ; }
-    tee() { gtee "${@}" ; }
-    base64() { gbase64 "${@}" ; }
-    md5sum() { gmd5sum "${@}" ; }
+    done
+}
 
+redirect_gnu_tools
