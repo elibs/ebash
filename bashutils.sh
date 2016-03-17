a31cdca802c44b3d820a7d9b69748c26fc5dd475
Michael Odell <michael@theodells.us>
2016-03-17 10:48:57 -0600

    Default TERM value and stop intercepting tput calls

--- bashutils.sh
+++ bashutils.sh
@@ -15,6 +15,8 @@ if [[ -e ${XDG_CONFIG_HOME:-${HOME:-}/.config}/bashutils.conf ]]; then
     source ${XDG_CONFIG_HOME:-${HOME:-}/.config}/bashutils.conf
 fi
 
+: ${TERM:=xterm-256color}
+
 # PLATFORM MUST BE FIRST.  It sets up aliases.  Those aliases won't be expanded
 # inside functions that are already declared, only inside those declared after
 # this.
