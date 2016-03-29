95af4b95ce2d44aab9c089c6e61c271c1f86d5c3
Michael Odell <michael@theodells.us>
2016-03-29 16:21:34 -0600

    Save exit code in _bashutils_on_exit_start more properly.

--- efuncs.sh
+++ efuncs.sh
@@ -751,9 +751,10 @@ _bashutils_on_exit_start()
     # Save off the exit code the fist time the exit trap is called -- it can be
     # reinvoked multiple times in the process of the shell going down.
     #
+    local exit_code=$?
     if [[ ! -v __BU_EXIT_CODE ]] ; then
         # Store off the exit code. This is used at the end of the exit trap inside _bashutils_on_exit_end.
-        __BU_EXIT_CODE=$?
+        __BU_EXIT_CODE=${exit_code}
         edebug "Bash process ${BASHPID} exited rc=${__BU_EXIT_CODE}"
         disable_signals
     fi
