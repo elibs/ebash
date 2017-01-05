a9ea5942f8868e4a238e329ec92ccc1aea22e15f
Marshall McMullen <marshall@solidfire.com>
2017-01-04 22:32:05 -0700

    Add --verbose to rm if eunmount is called with --verbose flag.

--- share/mount.sh
+++ share/mount.sh
@@ -342,6 +342,7 @@ eunmount()
 
             local rm_opts="--force"
             [[ ${recursive} -eq 1 ]] && rm_opts+=" --recursive"
+            [[ ${verbose}   -eq 1 ]] && rm_opts+=" --verbose"
 
             rm ${rm_opts} $(readlink -m "${mnt}")
         fi
