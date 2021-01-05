3f8dc11ea1c93445bde5322267753b21ac488856
Marshall McMullen <marshall.mcmullen@liqid.com>
2021-01-05 00:24:25 -0700

    Finally break up efuncs.sh into explicit modules and remove efuncs.sh entirely. YAY.

---
+++ share/filesystem.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#
+# Copyright 2011-2021, Marshall McMullen <marshall.mcmullen@gmail.com>
+# Copyright 2011-2018, SolidFire, Inc. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+# as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+# version.
+
+#-----------------------------------------------------------------------------------------------------------------------
+#
+# FILESYSTEM HELPERS
+#
+#-----------------------------------------------------------------------------------------------------------------------
+
+opt_usage pushd <<'END'
+Wrapper around pushd to suppress its noisy output.
+END
+pushd()
+{
+    builtin pushd "${@}" >/dev/null
+}
+
+opt_usage popd <<'END'
+Wrapper around popd to suppress its noisy output.
+END
+popd()
+{
+    builtin popd "${@}" >/dev/null
+}
+
+opt_usage command_exists <<'END'
+Helper function to check if a command exists. The actual implementation could be a function in
+our environment or an external program.
+END
+command_exists()
+{
+    { declare -f "${1}" || which "${1}"; } &>/dev/null
+}
+
+opt_usage require <<'END'
+Helper function to validate that a list of commands are all installed in our PATH.
+END
+require()
+{
+    local missing=0
+    local cmd
+    for cmd in "${@}"; do
+        if ! command_exists "${cmd}"; then
+            eerror "Command ${cmd} not found in ${PATH}"
+            (( missing += 1 ))
+        fi
+    done
+
+    assert_zero "${missing}"
+}
+
+opt_usage echmodown <<'END'
+echmodown is basically chmod and chown combined into one function.
+END
+echmodown()
+{
+    [[ $# -ge 3 ]] || die "echmodown requires 3 or more parameters. Called with $# parameters (chmodown $@)."
+    $(opt_parse mode owner)
+
+    chmod ${mode} $@
+    chown ${owner} $@
+}
+
+opt_usage efreshdir <<'END'
+Recursively unmount the named directories and remove them (if they exist) then create new ones.
+
+NOTE: Unlike earlier implementations, this handles multiple arguments properly.
+END
+efreshdir()
+{
+    local mnt
+    for mnt in "${@}"; do
+
+        [[ -z "${mnt}" ]] && continue
+
+        eunmount -a -r -d "${mnt}"
+        mkdir -p ${mnt}
+
+    done
+}
+
+opt_usage ebackup <<'END'
+Copies the given file to *.bak if it doesn't already exist.
+END
+ebackup()
+{
+    $(opt_parse src)
+
+    [[ -e "${src}" && ! -e "${src}.bak" ]] && cp -arL "${src}" "${src}.bak" || true
+}
+
+opt_usage erestore <<'END'
+Copies files previously backed up via ebackup to their original location.
+END
+erestore()
+{
+    $(opt_parse src)
+
+    [[ -e "${src}.bak" ]] && mv "${src}.bak" "${src}"
+}
+
+opt_usage directory_empty <<'END'
+Check if a directory is empty
+END
+directory_empty()
+{
+    $(opt_parse dir)
+    ! find "${dir}" -mindepth 1 -print -quit | grep -q .
+}
+
+opt_usage directory_not_empty <<'END'
+Check if a directory is not empty
+END
+directory_not_empty()
+{
+    $(opt_parse dir)
+    find "${dir}" -mindepth 1 -print -quit | grep -q .
+}
+
+return 0
