49d9ab6395a36a022638ae112443727541a7f34b
Marshall McMullen <marshall.mcmullen@gmail.com>
2021-02-11 15:20:44 -0700

    Addresses ebash issue 4 to add new etable_values function. Just for you Ray.

--- share/etable.sh
+++ share/etable.sh
@@ -330,3 +330,103 @@ __etable_internal_html()
     echo "    </tbody>"
     echo "</table>"
 }
+
+opt_usage etable_values <<'END'
+etable_values is a special-purpose wrapper around etable which makes it easier to create an etable with a provided list
+of values and formats it into two columns, one showing the KEY and one showing the VALUE. For each variable, if it is a
+string or an array it will be expanded into the value as you'd expect. If the variable is an associative array or a
+pack, the keys and values will be unpacked and displayed in the KEY/VALUE columns in an exploded manner. This function
+relies on print_value() for pretty printing individual values. Specifically:
+
+    1) Strings: "value with spaces"
+    2) Arrays:  ("value with spaces" "Something" "1")
+
+For example, if you passed in:
+
+    $ etable_values HOME USER
+
+This would produce
+
+    +------+------------------+
+    | Key  | Value            |
+    +------+------------------+
+    | HOME | "/home/marshall" |
+    | USER | "marshall"       |
+    +------+------------------+
+
+If you have an associative array:
+
+    $ declare -A data([key1]="value1" [key2]="value2")
+    $ etable_values data
+
+This would produce
+
+    +------+----------+
+    | Key  | Value    |
+    +------+----------+
+    | key1 | "value1" |
+    | key2 | "value2" |
+    +------+----------+
+
+Similar to ebanner there is an --uppercase and a --lowercase if you want to have all the keys in all uppercase or
+lowercase for consistency.
+END
+etable_values()
+{
+    local default_columns="Key|Value"
+    $(opt_parse \
+        ":style          | Table style (ascii, html, boxart). Default=ascii."         \
+        "+rowlines       | Display a separator line in between each row."             \
+        "+column_delim=1 | Display column delimiters."                                \
+        ":columns        | Column headers for the table (default=${default_columns}." \
+        "+uppercase      | Uppercase all keys for display consistency."               \
+        "+lowercase      | Lowercase all keys for display consistency."               \
+        "+sort=1         | Sort keys in the tabular output."                          \
+        "@entries        | Array of variables to display the values of.")
+
+    : ${columns:="${default_columns}"}
+    local rows=()
+
+    # Helper function to determine display key to use
+    display_key()
+    {
+        local original_key="${1}"
+        if [[ ${uppercase} -eq 1 ]]; then
+            echo "${original_key^^}"
+        elif [[ ${lowercase} -eq 1 ]]; then
+            echo "${original_key,,}"
+        else
+            echo "${original_key}"
+        fi
+    }
+
+    local __entry __key
+    for __entry in "${entries[@]}"; do
+
+        if is_associative_array "${__entry}"; then
+            for __key in $(array_indexes_sort ${__entry}); do
+                eval 'array_add_nl rows "'$(display_key ${__key})'|${'${__entry}'['${__key}']}"'
+            done
+
+        elif is_pack "${__entry}"; then
+            for __key in $(pack_keys_sort ${__entry#%}); do
+                array_add_nl rows "$(display_key ${__key})|$(pack_get ${__entry#%} ${__key})"
+            done
+        elif is_array "${__entry}"; then
+            array_add_nl rows "$(display_key ${__entry})|$(print_value ${__entry})"
+        else
+            eval 'array_add_nl rows "'$(display_key ${__entry})'|${'${__entry}'}"'
+        fi
+    done
+
+    if [[ ${sort} -eq 1 ]]; then
+        array_sort rows
+    fi
+
+    # We add in the column headers after we've added all the rows so it doesn't get sorted amongst the actual rows.
+    rows=( "${columns}" "${rows[@]}" )
+
+    # Forward everything to actual etable function.
+    opt_forward etable style rowlines column_delim -- "${rows[@]}"
+}
+
