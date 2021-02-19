09b3248e988a924f97264c2cc02ccf971ab8d4b4
Marshall McMullen <marshall.mcmullen@gmail.com>
2021-02-19 01:28:11 -0700

    Merge docker_build registry and repo into single option.

--- share/docker.sh
+++ share/docker.sh
@@ -31,7 +31,7 @@ This result in every developer having to do an initial build locally even if we'
 The algorithm we employ is as follows:
 
     1) Look for the image locally
-    2) Try to download the image from docker registry/repository
+    2) Try to download the image from docker repository
     3) Build the docker image from scratch
 
 This entire algorithm is built on a simple idea of essentially computingour own simplistic sha256 which corresponds to
@@ -39,16 +39,16 @@ the content of the provided Dockerfile as well as any files which are dynamicall
 directives in the Dockerfile. We then simply use that dynamically generated tag to easily be able to look for the image
 before we try to build.
 
-This function will create some output state files underneath ${workdir} that are super useful. These are all prefixed by
-the required ${repo}.
+This function will create some output state files underneath ${workdir}/docker that are super useful. These are all
+prefixed by ${name} which defaults to $(basename ${repo}).
 
-    1) ${repo}.options           : Options passed into docker_build
-    2) ${repo}.history           : Contains output of 'docker history'
-    3) ${repo}.inspect           : Contains output of 'docker inspect'
-    4) ${repo}.dockerfile        : Contains original dockerfile with all environment variables interpolated
-    5) ${repo}.${shafunc}        : Contains full content based sha of the dependencies to create the docker image
-    6) ${repo}.${shafunc}_short  : Contains first 12 characters of the full SHA of the dependencies of the image
-    7) ${repo}.${shafunc}_detail : Contains a detailed listing of all the dependencies that led to the creation of the
+    1) ${name}.options           : Options passed into docker_build
+    2) ${name}.history           : Contains output of 'docker history'
+    3) ${name}.inspect           : Contains output of 'docker inspect'
+    4) ${name}.dockerfile        : Contains original dockerfile with all environment variables interpolated
+    5) ${name}.${shafunc}        : Contains full content based sha of the dependencies to create the docker image
+    6) ${name}.${shafunc}_short  : Contains first 12 characters of the full SHA of the dependencies of the image
+    7) ${name}.${shafunc}_detail : Contains a detailed listing of all the dependencies that led to the creation of the
                                    docker image along with THEIR respective SHAs.
 
 NOTE: If you want to push any tags you need to provide --username and --password arguments or have DOCKER_USERNAME and
@@ -59,28 +59,28 @@ docker_build()
 {
     $(opt_parse \
         "&build_arg                     | Build arguments to pass into lower level docker build --build-arg."          \
-        "=registry                      | Name of docker registry for remote images."                                  \
-        "=repo                          | Name of docker repository for remote images."                                \
-        ":tags_url_base                 | Base docker URL to use to check for a tag. By default this uses dockerhub and
-                                          will appending your registry and repo to the base URL. By default the base URL
-                                          we use is 'https://hub.docker.com/v2/repositories' and we append a suffix of
-                                          '/tags'. For example, given a registry of 'liqid' and a repo of 'liqid' by
-                                          default we use 'https://hub.docker.com/v2/repositories/liqid/liqid/tags'"    \
-        ":tags_url_full                 | In the event you need more control over the URL used for looking up a docker
-                                          tag than offered by tags_url_base, you can give the fully qualified URL
-                                          we should use for looking up docker tagss."                                  \
         ":file=Dockerfile               | The docker file to use. Defaults to Dockerfile."                             \
-        "+pull                          | Pull the image from the remote registry."                                    \
-        "&push                          | List of tags to push to the remote registry. The special value 'builtin' is
-                                          used indicate you want to push the content-based tag that we auto generate." \
+        ":name                          | Name to use for generated artifacts. Defaults to the basename of repo."      \
+        "+pull                          | Pull the image from the remote registry/repo."                               \
+        "&push                          | List of tags to push to the remote registry/repo. The special value 'builtin'
+                                          indicates you want to push the content-based tag ebash auto generates."      \
         "+pretend                       | Do not actually build the docker image. Return 0 if image already exists and 1
                                           if the image does not exist and a build is required."                        \
+        "=repo                          | Name of docker regisrty/repository for remote images."                       \
         ":shafunc=sha256                | SHA function to use. Default to sha256."                                     \
         "&tag                           | Optional tags to assign to the image in addition to the content based SHA.
                                           Unlike normal docker, this is JUST the tag NOT name:tag."                    \
-        ":username=${DOCKER_USERNAME:-} | Username for pushing images to the docker registry. Defaults to DOCKER_USERNAME
+        ":tags_url_base                 | Base docker URL to use to check for a tag. By default this uses dockerhub and
+                                          will append your registry/repo to the base URL. The base URL we use is
+                                          'https://hub.docker.com/v2/repositories' and we append a suffix of '/tags'.
+                                          With a repo of 'liqid/liqid' we would thus use
+                                          'https://hub.docker.com/v2/repositories/liqid/liqid/tags'"                   \
+        ":tags_url_full                 | In the event you need more control over the URL used for looking up a docker
+                                          tag than offered by tags_url_base, you can give the fully qualified URL
+                                          we should use for looking up docker tagss."                                  \
+        ":username=${DOCKER_USERNAME:-} | Username for pushing images to the registry/repo. Defaults to DOCKER_USERNAME
                                           environment variable"                                                        \
-        ":password=${DOCKER_PASSWORD:-} | Password for pushing images to the docker registry. Defaults to DOCKER_PASSWORD
+        ":password=${DOCKER_PASSWORD:-} | Password for pushing images to the registry/repo. Defaults to DOCKER_PASSWORD
                                           environment variable"                                                        \
         ":workdir=.work/docker          | Temporary work directory to save output files to."                           \
     )
@@ -88,12 +88,13 @@ docker_build()
     mkdir -p "${workdir}"
     assert_exists "${file}"
 
-    local options="${workdir}/${repo}.options"
-    local history="${workdir}/${repo}.history"
-    local inspect="${workdir}/${repo}.inspect"
-    local shafile="${workdir}/${repo}.${shafunc}"
-    local shafile_short="${workdir}/${repo}.${shafunc}_short"
-    local shafile_detail="${workdir}/${repo}.${shafunc}_detail"
+    : ${name:="$(basename "${repo}")"}
+    local options="${workdir}/${name}.options"
+    local history="${workdir}/${name}.history"
+    local inspect="${workdir}/${name}.inspect"
+    local shafile="${workdir}/${name}.${shafunc}"
+    local shafile_short="${workdir}/${name}.${shafunc}_short"
+    local shafile_detail="${workdir}/${name}.${shafunc}_detail"
     opt_dump | sort > "${options}"
 
     # Add any build arguments into sha_detail
@@ -108,7 +109,7 @@ docker_build()
         build_arg_vals+=( "--build-arg ${entry}" )
     done
 
-    local dockerfile="${workdir}/${repo}.dockerfile"
+    local dockerfile="${workdir}/${name}.dockerfile"
     envsubst "$(array_join build_arg_keys ,)" < "${file}" > "${dockerfile}"
 
     # Strip out ARGs that we've interpolated
@@ -144,7 +145,7 @@ docker_build()
     echo "${sha_short}" > "${shafile_short}"
 
     # Image we should look for
-    image="${registry}/${repo}:${sha_short}"
+    image="${repo}:${sha_short}"
     edebug $(lval      \
         build_arg      \
         build_arg_keys \
@@ -156,7 +157,6 @@ docker_build()
         inspect        \
         pretend        \
         push           \
-        registry       \
         repo           \
         sha            \
         sha_short      \
@@ -187,9 +187,9 @@ docker_build()
         if [[ -n "${tags_url_full}" ]]; then
             docker_url="${tags_url_full}/${sha_short}/"
         elif [[ -n "${tags_url_base}" ]]; then
-            docker_url="${tags_url_base}/${registry}/${repo}/tags/${sha_short}/"
+            docker_url="${tags_url_base}/${repo}/tags/${sha_short}/"
         else
-            docker_url="https://hub.docker.com/v2/repositories/${registry}/${repo}/tags/${sha_short}/"
+            docker_url="https://hub.docker.com/v2/repositories/${repo}/tags/${sha_short}/"
         fi
 
         edebug "Checking remote $(lval docker_url)"
@@ -217,7 +217,7 @@ docker_build()
         [[ -z "${entry}" ]] && continue
 
         einfo "Tagging with custom $(lval tag=entry)"
-        docker build --tag "${registry}/${repo}:${entry}" --file "${dockerfile}" . | edebug
+        docker build --tag "${repo}:${entry}" --file "${dockerfile}" . | edebug
     done
 
     einfo "Size"
