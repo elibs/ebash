34794f82960e8e6637816f5f6b9183be1e901377
Build Automation <build.automation@liqid.com>
2021-04-01 17:41:05 -0600

    Put echo statement back where it was originally.

--- share/emock.sh
+++ share/emock.sh
@@ -148,13 +148,14 @@ emock()
             called=$(cat "${statedir}/called")
             (( called++ ))
         fi 
-        # Create directory to store files in for this invocation
+        echo ${called} > "${statedir}/called"
+ 
+       # Create directory to store files in for this invocation
         mkdir -p "${statedir}/${called}"
 
         # Save off timestamp and argument array
         echo -en $(date "+%FT%TZ") > "${statedir}/${called}/timestamp"
         printf "\"%s\" " "${@}" > "${statedir}/${called}/args"
-        echo ${called} > "${statedir}/called"
     '
 
     # Create the mock
