00d6caea76f9b23a5ce97967f48478b8f6dd2f30
Marshall McMullen <marshall.mcmullen@liqid.com>
2021-04-08 23:30:00 -0600

    Remove ebash --install functions and just make these standalone scripts again.

--- share/ebash.sh
+++ share/ebash.sh
@@ -81,6 +81,19 @@ unset CDPATH
 # Automatic documentation
 #
 #-----------------------------------------------------------------------------------------------------------------------
+# All functions and ebash based scripts which utilize `opt_parse` get automatic documentation and usage information.
+# This is automatically available via `--help` or `-?` options. For this to work, we have to store off the docstrings
+# above each function into variables for subsequent output when the usage is requested via `--help` or `-?`.
+#
+# But we don't want to bloat the interpreter with a bunch of documentation every time we source ebash. Our solution is
+# to only save them at times where we believe the variables are going to be needed. There's no reason to expect they
+# will be necessary unless `--help` or `-?` is on the command line somewhere. Or for those few cases where it's needed,
+# for any other reason, the caller can just set `__EBASH_SAVE_DOC=1` explicitly.
+for arg in "$@" ; do
+    if [[ ${arg} == "--help" || ${arg} == "-?" ]] ; then
+        __EBASH_SAVE_DOC=1
+    fi
+done
 
 # This function is used to create documentation for bash functions that can be queried at runtime. To avoid bloating
 # the interpreter for this all the time, it is only performed when __EBASH_SAVE_DOC is set to 1.
