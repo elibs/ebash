119f53ed52a89570e7c6255aaaec24c5d23fd23d
Marshall McMullen <marshall.mcmullen@gmail.com>
2021-06-30 12:49:29 -0600

    Fix lint error in elogfile.

--- share/elogfile.sh
+++ share/elogfile.sh
@@ -112,6 +112,8 @@ elogfile()
                 nodie_on_error
 
                 if [[ ${tail} -eq 1 ]]; then
+                    # shellcheck disable=SC2261
+                    # This tee is carefully constructed to send stdout to a named file descriptor so disable shellcheck
                     tee -a "${files[@]}" <${pipe} >&$(get_stream_fd ${name}) 2>/dev/null
                 else
                     tee -a "${files[@]}" <${pipe} >/dev/null 2>&1
