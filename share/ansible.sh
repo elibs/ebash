830379262ae2d14ea678b9b17e240ac508c0a273
Michael Kirby <michael.kirby@liqid.com>
2021-08-05 15:47:01 -0600

    Add initial ansible module
    Adds an initial ansible module with a single helper function to output expected Ansible
    JSON status format.

---
+++ share/ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright 2021, Marshall McMullen <marshall.mcmullen@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+# as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+# version.
+
+opt_usage ansible_status <<'END'
+`ansible_status` is used to relay status back to Ansible in expected JSON format for completed tasks.
+This prints status for Ansible in the following format -
+{"failed": <true|false> "changed": <true|false> "msg": <user provided message>}
+END
+ansible_status()
+{
+    $(opt_parse \
+        "=failed    f   | Did this module fail - true | false."             \
+        "=changed   c   | Did this module change anything - true | false."  \
+        "=msg       m   | Message associated with this status.")
+
+    # Note - Ansible is particular about it's status output. We need to escape
+    #        any characters with special meaning in JSON.
+    msg=$(json_escape "${msg}")
+    printf '{"failed": %s, "changed": %s, "msg": %s}' "${failed}" "${changed}" "${msg}"
+}
