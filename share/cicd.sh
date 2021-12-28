47595d898f5ecfc1fcdfedf83069190fdb38f62d
Marshall McMullen <marshall.mcmullen@gmail.com>
2021-12-27 18:06:27 -0700

    Deal with CI/CD versioning when no tags are present.

--- share/cicd.sh
+++ share/cicd.sh
@@ -46,12 +46,17 @@ cicd_info()
         "pack | Name of the pack to fill in with CICD details." \
     )
 
+    # Parse the version_tag using the provided EBASH_CICD_TAG_MATCH. If we are unable to find a match use a
+    # corresponding default tag.
     local version_tag version base_tag
-    version_tag=$(git describe --always --tags --match "${EBASH_CICD_TAG_MATCH}" --abbrev=10)
+    if ! version_tag=$(git describe --tags --match "${EBASH_CICD_TAG_MATCH}" --abbrev=10 2>/dev/null); then
+        version_tag="${EBASH_CICD_TAG_MATCH//\*/0}"
+    fi
+
+    # Now we can safely parse out the component version parts
+    local parts=()
     version="${version_tag#v}"
     base_tag="${version_tag%%-*}"
-
-    local parts=()
     array_init parts "${version%%-*}" "."
     edebug "$(lval parts)"
 
@@ -65,20 +70,20 @@ cicd_info()
     fi
 
     pack_set "${pack}" \
-        base_tag="${base_tag}"                                                 \
-        branch="$(git branch --show-current)"                                  \
-        build="${parts[3]:-0}"                                                 \
-        commit="$(git rev-parse HEAD)"                                         \
-        commit_short="$(string_truncate 10 $(git rev-parse HEAD))"             \
-        major="${parts[0]}"                                                    \
-        minor="${parts[1]}"                                                    \
-        offset="$(git rev-list ${base_tag}..HEAD --count)"                     \
-        origin_url="$(git config --get remote.origin.url)"                     \
-        patch="${parts[2]}"                                                    \
-        repo_slug="$(basename "$(git config --get remote.origin.url)" ".git")" \
-        series="${parts[0]}.${parts[1]}"                                       \
-        version="${version}"                                                   \
-        version_tag="${version_tag}"                                           \
+        base_tag="${base_tag}"                                                      \
+        branch="$(git branch --show-current)"                                       \
+        build="${parts[3]:-0}"                                                      \
+        commit="$(git rev-parse HEAD)"                                              \
+        commit_short="$(string_truncate 10 $(git rev-parse HEAD))"                  \
+        major="${parts[0]}"                                                         \
+        minor="${parts[1]}"                                                         \
+        offset="$(git rev-list ${base_tag}..HEAD --count 2>/dev/null || echo 0)"    \
+        origin_url="$(git config --get remote.origin.url)"                          \
+        patch="${parts[2]}"                                                         \
+        repo_slug="$(basename "$(git config --get remote.origin.url)" ".git")"      \
+        series="${parts[0]}.${parts[1]}"                                            \
+        version="${version}"                                                        \
+        version_tag="${version_tag}"                                                \
         version_tag_next="${version_tag_next}"
 }
 
