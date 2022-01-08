884a9a8a1c7b693e3e245bafe588fa0a415432f5
Marshall McMullen <marshall.mcmullen@gmail.com>
2022-01-07 17:00:14 -0700

    Fix lint error. Caught a real bug!

--- share/emock.sh
+++ share/emock.sh
@@ -821,7 +821,7 @@ emock_dump_all_state()
     )
 
     local entry
-    for entry in "$(find "${statedir}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")"; do
+    for entry in $(find "${statedir}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n"); do
         ewarn "Found: $(lval statedir entry)"
         opt_forward emock_dump_state statedir -- "${entry}"
     done
