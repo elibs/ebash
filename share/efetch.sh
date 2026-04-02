a1b49f5e38ef4b5fc3a9b584e9715575653dbb56
Marshall McMullen <marshall.mcmullen@gmail.com>
2026-04-02 02:56:42 -0600

    WIP

--- share/efetch.sh
+++ share/efetch.sh
@@ -404,8 +404,9 @@ efetch_wait()
     fi
 
     # Wait for tail to complete. That process will stop gracefully when the process we are tailing exits.
+    # Ignore tail's exit code since it may fail if the output file is deleted (expected with delete_output=1).
     if [[ -n "${tail_pid}" ]]; then
-        wait ${tail_pid}
+        wait ${tail_pid} || true
     else
 
         if [[ "${progress}" -eq 1 ]]; then
