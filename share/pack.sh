361d23738ce3f7db024131e9f2d52494df8299ee
Marshall McMullen <marshall.mcmullen@gmail.com>
2026-04-02 12:14:34 -0600

    More portable _pack.

--- share/pack.sh
+++ share/pack.sh
@@ -379,8 +379,7 @@ _unpack()
 _pack()
 {
     # NOTE: BSD base64 is really chatty and this is the reason we discard its error output
-    # Filter empty lines and convert newlines to nulls in one process
-    awk '/./ {printf "%s\0", $0}' | base64 2>/dev/null
+    grep -av '^$' | tr '\n' '\0' | base64 2>/dev/null
 }
 
 pack_encode()
